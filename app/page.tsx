7eb61c0ce9adf5e7c3f75a52782f99cb9064c38d
viniciusbr32 <viniciusgba2008@gmail.com>
2024-08-06 19:03:05 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import { Header } from "./_components/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
-      <Button>Teste</Button>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Vinicius</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.svg"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
