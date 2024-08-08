6691f9aa4cf993d3f2eaa574f82636e38a1f3af7
viniciusbr32 <viniciusgba2008@gmail.com>
2024-08-08 11:01:59 -0300

    feat: add barbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,12 @@ import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import { BarbershopItem } from "./_components/barbershop-item"
 
-export default function Home() {
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       {/* Header */}
@@ -36,8 +40,10 @@ export default function Home() {
         </div>
 
         {/* Agendamentos */}
-
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* Div Esquerda */}
             <div className="flex flex-col gap-2 py-5 pl-5">
@@ -59,7 +65,17 @@ export default function Home() {
             </div>
           </CardContent>
         </Card>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="scrollbar-hide flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
 }
+
+export default Home
